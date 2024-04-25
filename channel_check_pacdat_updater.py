2e68de52a492ab7d67a8e79d8ceaad51d8fea75f
Christian Richard <lifepupil@gmail.com>
2024-04-25 13:19:28 -0400

    fixed issue creating empty columns for pacdat updater for HPC

--- channel_check_pacdat_updater.py
+++ channel_check_pacdat_updater.py
@@ -30,38 +30,39 @@ pac_dir = os.environ['TMPDIR'] + '/input/'
 
 pacdat = pd.read_pickle(pac_dir + which_pacdat)
 
+
 if not(any([c=='max_flat_slip'+str(slip_f_cutoff) for c in pacdat.columns])):
     # pacdat.insert(4,'max_flat_slip'+str(slip_f_cutoff), np.ones(len(pacdat))*99999)
-    pacdat[['max_flat_slip'+str(slip_f_cutoff)]] = np.ones(len(pacdat))*99999
+    pacdat['max_flat_slip'+str(slip_f_cutoff)] = np.ones(len(pacdat))*99999
     
 if not(any([c=='perc_flat_slip'+str(slip_f_cutoff) for c in pacdat.columns])):    
     # pacdat.insert(4,'perc_flat_slip'+str(slip_f_cutoff), np.ones(len(pacdat))*99999)
-    pacdat[['perc_flat_slip'+str(slip_f_cutoff)]] = np.ones(len(pacdat))*99999
+    pacdat['perc_flat_slip'+str(slip_f_cutoff)] = np.ones(len(pacdat))*99999
 
 if not(any([c=='N_flat_slip'+str(slip_f_cutoff) for c in pacdat.columns])):
     # pacdat.insert(4,'N_flat_slip'+str(slip_f_cutoff), np.ones(len(pacdat))*99999)
-    pacdat[['N_flat_slip'+str(slip_f_cutoff)]] = np.ones(len(pacdat))*99999
+    pacdat['N_flat_slip'+str(slip_f_cutoff)] = np.ones(len(pacdat))*99999
     
 if not(any([c=='avg_flat_slip'+str(slip_f_cutoff) for c in pacdat.columns])):
     # pacdat.insert(4,'avg_flat_slip'+str(slip_f_cutoff), np.ones(len(pacdat))*99999)
-    pacdat[['avg_flat_slip'+str(slip_f_cutoff)]] = np.ones(len(pacdat))*99999
+    pacdat['avg_flat_slip'+str(slip_f_cutoff)] = np.ones(len(pacdat))*99999
     
     
 if not(any([c=='max_noise_slip'+str(slip_n_cutoff) for c in pacdat.columns])):
     # pacdat.insert(4,'max_noise_slip'+str(slip_n_cutoff), np.ones(len(pacdat))*99999)  
-    pacdat[['max_noise_slip'+str(slip_n_cutoff)]] = np.ones(len(pacdat))*99999
+    pacdat['max_noise_slip'+str(slip_n_cutoff)] = np.ones(len(pacdat))*99999
     
 if not(any([c=='perc_noise_slip'+str(slip_n_cutoff) for c in pacdat.columns])):
     # pacdat.insert(4,'perc_noise_slip'+str(slip_n_cutoff), np.ones(len(pacdat))*99999) 
-    pacdat[['perc_noise_slip'+str(slip_n_cutoff)]] = np.ones(len(pacdat))*99999
+    pacdat['perc_noise_slip'+str(slip_n_cutoff)] = np.ones(len(pacdat))*99999
     
 if not(any([c=='avg_noise_slip'+str(slip_n_cutoff) for c in pacdat.columns])):
     # pacdat.insert(4,'avg_noise_slip'+str(slip_n_cutoff), np.ones(len(pacdat))*99999) 
-    pacdat[['avg_noise_slip'+str(slip_n_cutoff)]] = np.ones(len(pacdat))*99999
+    pacdat['avg_noise_slip'+str(slip_n_cutoff)] = np.ones(len(pacdat))*99999
     
 if not(any([c=='N_noise_slip'+str(slip_n_cutoff) for c in pacdat.columns])):
     # pacdat.insert(4,'N_noise_slip'+str(slip_n_cutoff), np.ones(len(pacdat))*99999) 
-    pacdat[['N_noise_slip'+str(slip_n_cutoff)]] = np.ones(len(pacdat))*99999
+    pacdat['N_noise_slip'+str(slip_n_cutoff)] = np.ones(len(pacdat))*99999
     
     
 # pacdat.insert(2,'max_flat', np.zeros(len(pacdat)))
